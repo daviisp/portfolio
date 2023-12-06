707b3c0c5df8364fc96e8b0e1e325791bf9e41a5
Davi Silva <davi.pimenta8@gmail.com>
2023-12-05 22:54:15 -0300

    Primeiro commit

---
+++ public/src/script.js
@@ -0,0 +1,18 @@
+let openButton = document.getElementById('open-button')
+let closeButton = document.getElementById('close-button')
+let overlayMenu = document.getElementById('overlay-menu')
+let menu = document.getElementById('menu-mobile')
+
+openButton.addEventListener('click', () => {
+    menu.classList.add('menu-mobile-show')
+})
+
+closeButton.addEventListener('click', () => {
+    menu.classList.remove('menu-mobile-show')
+})
+
+overlayMenu.addEventListener('click', () => {
+    menu.classList.remove('menu-mobile-show')
+})
+
+
